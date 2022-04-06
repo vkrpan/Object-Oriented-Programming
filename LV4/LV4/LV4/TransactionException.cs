a73c4645e43511e6faf347139c8f4302cd8b69e6
vkrpan <vatroslav.krpan1@gmail.com>
2022-04-06 13:54:43 +0200

    LV4 added

---
+++ LV4/LV4/LV4/TransactionException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV4
+{
+    class TransactionException : Exception
+    {
+        public TransactionException(string message) : base(message)
+        {
+            Console.WriteLine("Iznimka uhvacena");
+        }
+    }
+}
